80ffc03d135b94ac8b273f5f0726afbd9cf36c90
upnormalmekah <106486766+upnormalmekah@users.noreply.github.com>
2024-12-22 05:43:13 +0700

    Update storeData.js

--- src/services/storeData.js
+++ src/services/storeData.js
@@ -1,10 +1,19 @@
 const { Firestore } = require('@google-cloud/firestore');
- 
+const pathKey = path.resolve('./submissionmlgc-nabil-445409-1fc3a4d2f98d.json')
+     
 async function storeData(id, data) {
-  const db = new Firestore();
+
+  try {
+    const db = new Firestore({
+      projectId: 'submissionmlgc-nabil-445409',
+      keyFilename: pathKey,
+    });
  
-  const predictCollection = db.collection('predictions');
-  return predictCollection.doc(id).set(data);
+    const predictCollection = db.collection('predictions');
+    return predictCollection.doc(id).set(data);
+  }catch(error) {
+    console.error(error);
+  }
 }
  
 module.exports = storeData;
