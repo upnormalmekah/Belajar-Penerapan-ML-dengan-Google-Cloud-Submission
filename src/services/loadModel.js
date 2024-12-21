374109b20e8f0d29cdfcb439cda2e67223866252
upnormalmekah <106486766+upnormalmekah@users.noreply.github.com>
2024-12-22 05:32:54 +0700

    Update loadModel.js

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -1,5 +1,5 @@
 const tf = require('@tensorflow/tfjs-node');
 async function loadModel() {
-    return tf.loadGraphModel(process.env.MODEL_URL);
+    return tf.loadGraphModel("https://storage.googleapis.com/cancer-storage-submissionmlgc/model.json");
 }
 module.exports = loadModel;
